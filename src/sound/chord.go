45514f553fa801cbb8a8ec949f60fff7b910009b
sawadyecma <snowslidez@yahoo.co.jp>
2021-12-10 20:22:11 +0900

    add: consistNote

--- src/sound/chord.go
+++ src/sound/chord.go
@@ -8,14 +8,23 @@ import (
 type chord struct {
 	// not lowest note
 	rootNote absnote
-	notes    []absnote
+	notes    []consistNote
+}
+
+type consistNote struct {
+	absnote
+	interval int
 }
 
 func NewChord(rootNote Absnote, notes []Absnote) Chord {
 
-	noteVals := make([]absnote, len(notes))
+	noteVals := make([]consistNote, len(notes))
 	for i := range notes {
-		noteVals[i] = reflect.ValueOf(notes[i]).Interface().(absnote)
+		an := reflect.ValueOf(notes[i]).Interface().(absnote)
+		noteVals[i] = consistNote{
+			absnote:  an,
+			interval: rootNote.Diff(an),
+		}
 	}
 	return chord{
 		rootNote: reflect.ValueOf(rootNote).Interface().(absnote),
@@ -25,5 +34,28 @@ func NewChord(rootNote Absnote, notes []Absnote) Chord {
 
 func (c chord) Name() string {
 	// example: C Cm CM7 Cm7 C7 C9 Cadd9 Cm9 C11 C13 C7/E
+
+	// 分数コード
+	// ルートより低い音が1つあれば、分数コード
+	// ルートより低い音が2つ以上あったら命名しない
+	lowerRoots := c.getLowerNoteByRoot()
+	if len(lowerRoots) == 1 {
+		return fmt.Sprintf("%s/%s", c.rootNote, lowerRoots[0].absnote)
+	}
+
+	if len(lowerRoots) == 2 {
+		panic("invalid on chord")
+	}
+
 	return fmt.Sprintf("%s", c.rootNote)
+}
+
+func (c chord) getLowerNoteByRoot() []consistNote {
+	ret := make([]consistNote, 0, len(c.notes))
+	for i := range c.notes {
+		if c.rootNote.Diff(c.notes[i].absnote) < 0 {
+			ret = append(ret, c.notes[i])
+		}
+	}
+	return ret
 }
--- src/sound/chord_test.go
+++ src/sound/chord_test.go
@@ -13,7 +13,12 @@ func Example_chord_Name() {
 		NewChord(E3, []Absnote{E3, G3}).Name(),
 	)
 
+	fmt.Println(
+		NewChord(C3, []Absnote{E2, G3}).Name(),
+	)
+
 	// Output:
 	// C3
 	// E3
+	// C3/E2
 }
