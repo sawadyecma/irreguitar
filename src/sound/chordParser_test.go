bb06ac2ed5234fa9e525783c8e73a2cbf0846a22
sawadyecma <snowslidez@yahoo.co.jp>
2021-12-12 12:42:40 +0900

    move: parsed

--- src/sound/chordParser_test.go
+++ src/sound/chordParser_test.go
@@ -14,6 +14,7 @@ func Test_chordParser_Parse(t *testing.T) {
 		notes []Absnote
 		want  string
 	}{
+		{C3, []Absnote{}, "C"},
 		{C3, []Absnote{E3, G3}, "C"},
 		{C3, []Absnote{Eb3, G3}, "Cm"},
 		{C3, []Absnote{E3, G3, B3}, "CM7"},
@@ -29,47 +30,6 @@ func Test_chordParser_Parse(t *testing.T) {
 				got := p.Parse(test.root, test.notes)
 				if !reflect.DeepEqual(got, test.want) {
 					t.Errorf("want: %s, got: %s", test.want, got)
-				}
-			},
-		)
-	}
-}
-
-func Test_parsed_ChordType(t *testing.T) {
-	tests := []struct {
-		parsed parsed
-		want   string
-	}{
-		{
-			parsed: parsed{
-				thirdType:   lenTypeMajor,
-				seventhType: lenTypeMajor,
-			},
-			want: "M7",
-		},
-		{
-			parsed: parsed{
-				thirdType:   lenTypeMajor,
-				seventhType: lenTypeMinor,
-			},
-			want: "7",
-		},
-		{
-			parsed: parsed{
-				thirdType:   lenTypeMajor,
-				seventhType: lenTypeNone,
-			},
-			want: "",
-		},
-	}
-
-	for i, tt := range tests {
-		t.Run(
-			fmt.Sprintf("test:%d", i+1),
-			func(t *testing.T) {
-				got := tt.parsed.ChordType()
-				if !reflect.DeepEqual(got, tt.want) {
-					t.Errorf("want: %s, got: %s", tt.want, got)
 				}
 			},
 		)
