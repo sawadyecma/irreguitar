f11c63a27297b71709698ffb66ee342744ac56cc
sawadyecma <snowslidez@yahoo.co.jp>
2021-12-12 13:02:54 +0900

    add: judgeLenType

--- src/chord/consistNote.go
+++ src/chord/consistNote.go
@@ -9,6 +9,16 @@ type consistNote struct {
 
 type consistNotes []consistNote
 
+func (r consistNotes) filter(judge func(ele consistNote) bool) consistNotes {
+	ret := make([]consistNote, 0, len(r))
+	for i := range r {
+		if judge(r[i]) {
+			ret = append(ret, r[i])
+		}
+	}
+	return ret
+}
+
 func (r consistNotes) lowersByRoot(root sound.Absnote) consistNotes {
 	return r.filter(func(ele consistNote) bool {
 		return root.Diff(ele.note) < 0
@@ -27,12 +37,31 @@ func (r consistNotes) matchIntervals(root sound.Absnote, interval int) consistNo
 	})
 }
 
-func (r consistNotes) filter(judge func(ele consistNote) bool) consistNotes {
-	ret := make([]consistNote, 0, len(r))
-	for i := range r {
-		if judge(r[i]) {
-			ret = append(ret, r[i])
+func (r consistNotes) getThirdType(root sound.Absnote) lenType {
+	containMinor := len(r.matchIntervals(root, 3)) != 0
+	containMajor := len(r.matchIntervals(root, 4)) != 0
+
+	return judgeLenType(containMinor, containMajor)
+}
+
+func (r consistNotes) getSeventhType(root sound.Absnote) lenType {
+	containMinor := len(r.matchIntervals(root, 10)) != 0
+	containMajor := len(r.matchIntervals(root, 11)) != 0
+
+	return judgeLenType(containMinor, containMajor)
+}
+
+func judgeLenType(containMinor bool, containMajor bool) lenType {
+	if containMajor {
+		if containMinor {
+			return lenTypeInvalid
 		}
+		return lenTypeMajor
 	}
-	return ret
+
+	if containMinor {
+		return lenTypeMinor
+	}
+
+	return lenTypeNone
 }
